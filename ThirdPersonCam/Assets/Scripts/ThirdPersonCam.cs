75daed97ad3de7d7b33fb4fc4851fa90289212bb
谢刘建 <377624289@qq.com>
2017-05-19 00:27:05 +0800

    ThirdPersonCam

--- ThirdPersonCam/Assets/Scripts/ThirdPersonCam.cs
+++ ThirdPersonCam/Assets/Scripts/ThirdPersonCam.cs
@@ -2,15 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ThirdPersonCam : MonoBehaviour {
+public class ThirdPersonCam : MonoBehaviour
+{
+    #region 变量
 
-	// Use this for initialization
+    /// <summary>
+    /// 摄像机
+    /// </summary>
+    private Camera mCamera;
+
+    /// <summary>
+    /// 玩家transform
+    /// </summary>
+    public Transform mPlayer;
+
+    /// <summary>
+    /// 水平瞄准速度
+    /// </summary>
+    public float mHorizontalAimingSpeed = 400.0f;
+
+    /// <summary>
+    /// 垂直瞄准速度
+    /// </summary>
+    public float mVerticalAimingSpeed = 400.0f;
+
+    #endregion
+
+    #region 内置函数
+
+    void Awake()
+    {
+        mCamera = GetComponent<Camera>();
+    }
+
+    // Use this for initialization
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+    }
+
+    void LateUpdate()
+    {
+
+    }
+
+    #endregion
 }
