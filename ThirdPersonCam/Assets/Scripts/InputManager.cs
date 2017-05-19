06a7c348604197f3fdfb49d1db8bbcfdb2c06e29
谢刘建 <377624289@qq.com>
2017-05-19 12:49:06 +0800

    ThirdPersonCam

---
+++ ThirdPersonCam/Assets/Scripts/InputManager.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class InputManager : MonoBehaviour
+{
+    
+    bool useTouch;
+    bool useMouse;
+
+    public static Action<Touch[]> touchesBegin;
+    public static Action<Touch[]> touchesEnd;
+    public static Action<Touch[]> touchesMove;
+    public static Action<Touch[]> touchesTap;
+    
+    void Awake()
+    {
+        if ( Application.platform == RuntimePlatform.Android ||
+             Application.platform == RuntimePlatform.IPhonePlayer)
+        {
+            useTouch = true;
+            useMouse = false;
+        }
+        else
+        {
+            useMouse = true;
+            useTouch = false;
+        }
+    }
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (useTouch)
+        {
+            ProcessTouches();
+        }      
+    }
+
+    void ProcessTouches()
+    {
+        if(Input.touchCount > 0)
+        {
+            foreach (Touch tc in Input.touches)
+            {
+                if(tc.phase == TouchPhase.Began && touchesBegin != null)
+                {
+                    touchesBegin(Input.touches);
+                }
+
+                if (tc.phase == TouchPhase.Moved && touchesMove != null)
+                {
+                    touchesMove(Input.touches);
+                }
+
+                if (tc.phase == TouchPhase.Ended && touchesEnd != null)
+                {
+                    touchesEnd(Input.touches);
+                }
+
+                if (tc.phase == TouchPhase.Canceled && touchesEnd != null)
+                {
+                    touchesEnd(Input.touches);
+                }
+            }
+        }
+    }
+}
--- ThirdPersonCam/Assets/Scripts/ThirdPersonCam.cs
+++ ThirdPersonCam/Assets/Scripts/ThirdPersonCam.cs
@@ -8,7 +8,9 @@ public class ThirdPersonCam : MonoBehaviour
 
     public const string INPUT_MOUSE_X = "Mouse X";
     public const string INPUT_MOUSE_Y = "Mouse Y";
-    public const string ERROR_UNBindCam = "ThirdPersonCam脚本没有绑定摄像机!";
+    public const string INPUT_MOUSE_SCROLLWHEEL = "Mouse ScrollWheel";
+    public const string ERROR_UN_BINDCAM = "ThirdPersonCam脚本没有绑定摄像机!";
+    public const string ERROR_UN_PLAYER = "ThirdPersonCam脚本没有指定玩家";
 
     #endregion
 
@@ -55,6 +57,21 @@ public class ThirdPersonCam : MonoBehaviour
     public float mMinVerticalAngle = -60.0f;
 
     /// <summary>
+    /// 基础摄像机偏移的倍率的最大值
+    /// </summary>
+    public float mMaxDistance = 3.0f;
+
+    /// <summary>
+    /// 基础摄像机偏移的倍率的最小值
+    /// </summary>
+    public float mMinDistance = 1.0f;
+
+    /// <summary>
+    /// 镜头推进的速度
+    /// </summary>
+    public float mZoomSpeed = 2.0f;
+
+    /// <summary>
     /// 水平旋转的角度
     /// </summary>
     private float mAngleH = 0.0f;
@@ -63,6 +80,14 @@ public class ThirdPersonCam : MonoBehaviour
     /// 垂直旋转的角度
     /// </summary>
     private float mAngleV = 0.0f;
+
+    /// <summary>
+    /// 基础摄像机偏移的倍率
+    /// </summary>
+    private float mDistance = 0.0f;
+
+    private Vector2 mTouchMove = Vector2.zero;
+    private Vector2 mTouchLastPos = Vector2.zero;
 
     #endregion
 
@@ -74,42 +99,112 @@ public class ThirdPersonCam : MonoBehaviour
     }
 
     // Use this for initialization
-	void Start () {
-		
-	}
+	void Start ()
+    {
+        InputManager.touchesBegin += TouchesBegin;
+        InputManager.touchesMove += TouchesMoved;
+        InputManager.touchesEnd += TouchesEnd;
+    }
 	
 	// Update is called once per frame
 	void Update () {
 
     }
 
+    void OnDestroy()
+    {
+        InputManager.touchesBegin -= TouchesBegin;
+        InputManager.touchesMove -= TouchesMoved;
+        InputManager.touchesEnd -= TouchesEnd;
+    }
+
     void LateUpdate()
     {
         if (mCamera == null)
-            Debug.LogError(ERROR_UNBindCam);
-
-        if (mPlayer != null)
         {
+            Debug.LogError(ERROR_UN_BINDCAM);
+            return;
+        }
+
+        if (mPlayer == null)
+        {
+            Debug.LogError(ERROR_UN_PLAYER);
+            return;
+        }
+
 #if UNITY_EDITOR
-            if (Input.GetMouseButton(0))
-            {
-                mAngleH += Mathf.Clamp(Input.GetAxis(INPUT_MOUSE_X), -1, 1) * mHorizontalAimingSpeed * Time.deltaTime;
-                mAngleV += Mathf.Clamp(Input.GetAxis(INPUT_MOUSE_Y), -1, 1) * mVerticalAimingSpeed * Time.deltaTime;
-            }
+        if (Input.GetMouseButton(0))
+        {
+            mAngleH += Mathf.Clamp(Input.GetAxis(INPUT_MOUSE_X), -1, 1) * mHorizontalAimingSpeed * Time.deltaTime;
+            mAngleV += Mathf.Clamp(Input.GetAxis(INPUT_MOUSE_Y), -1, 1) * mVerticalAimingSpeed * Time.deltaTime;
+        }
 #endif
 
 #if UNITY_ANDROID || UNITY_IOS
-
+        mAngleH += Mathf.Clamp(mTouchMove.x / Screen.width, -1, 1) * mHorizontalAimingSpeed;
+        mAngleV += Mathf.Clamp(mTouchMove.y / Screen.height, -1, 1) * mVerticalAimingSpeed;
 #endif
 
-            mAngleV = Mathf.Clamp(mAngleV, mMinVerticalAngle, mMaxVerticalAngle);
-        }
+        mAngleV = Mathf.Clamp(mAngleV, mMinVerticalAngle, mMaxVerticalAngle);
 
         Quaternion animRotation = Quaternion.Euler(-mAngleV, mAngleH, 0.0f);
         Quaternion camYRotation = Quaternion.Euler(0.0f, mAngleH, 0.0f);
 
+        mDistance -= Input.GetAxis(INPUT_MOUSE_SCROLLWHEEL) * mZoomSpeed;
+        mDistance = Mathf.Clamp(mDistance, mMinDistance, mMaxDistance);
+
         mCamera.rotation = animRotation;
-        mCamera.position = mPlayer.position + camYRotation * mPivotOffset + animRotation * mCamOffset;
+        mCamera.position = mPlayer.position + camYRotation * mPivotOffset + animRotation * mCamOffset * mDistance;
+    }
+
+    #endregion
+
+    #region 回调函数
+
+    private void TouchesBegin(Touch[] touches)
+    {
+        foreach (var touch in touches)
+        {
+            if (touches.Length == 1)
+            {
+                mTouchLastPos = touch.position;
+            }
+            else if (touches.Length == 2)
+            {
+
+            }
+        }
+    }
+
+    private void TouchesMoved(Touch[] touches)
+    {
+        foreach (var touch in touches)
+        {
+            if (touches.Length == 1)
+            {
+                mTouchMove = touch.position - mTouchLastPos;
+                mTouchLastPos = touch.position;
+            }
+            else if (touches.Length == 2)
+            {
+
+            }
+        }
+    }
+
+    private void TouchesEnd(Touch[] touches)
+    {
+        foreach (var touch in touches)
+        {
+            if (touches.Length == 1)
+            {
+                mTouchMove = Vector2.zero;
+            }
+            else if (touches.Length == 2)
+            {
+
+            }
+        }
     }
 
     #endregion
