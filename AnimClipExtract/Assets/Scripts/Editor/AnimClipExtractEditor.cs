202eb38915d916d9ecf53b5e7fac8d14739971ed
谢刘建 <377624289@qq.com>
2017-05-26 08:52:45 +0800

    AnimClipExtract导入Max够选掉Embed Media属性

--- AnimClipExtract/Assets/Scripts/Editor/AnimClipExtractEditor.cs
+++ AnimClipExtract/Assets/Scripts/Editor/AnimClipExtractEditor.cs
@@ -15,6 +15,13 @@ public class AnimClipExtractEditor : Editor
     [MenuItem("Game/Anim/ExtractOne")]
     static void ExtractOne()
     {
+        if (Selection.objects.Length == 0)
+            return;
 
+        Object obj = Selection.objects[0];
+        string name = obj.name;
+        string path = AssetDatabase.GetAssetPath(obj);
+
+        int i = 0;
     }
 }
