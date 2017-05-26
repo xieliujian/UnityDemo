a5c65c8810ef303fd1b174658f3321ee502a39db
谢刘建 <377624289@qq.com>
2017-05-26 11:58:13 +0800

    AnimClipExtract不自动导出fbm文件

--- AnimClipExtract/Assets/Scripts/Editor/AnimClipExtractEditor.cs
+++ AnimClipExtract/Assets/Scripts/Editor/AnimClipExtractEditor.cs
@@ -3,9 +3,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using System.IO;
 
 public class AnimClipExtractEditor : Editor
 {
+    private const string ANIM_PATH = "Assets/Art/3D/Characters/";
+    private const string PREFABS_PATH = "Assets/Prefabs/3D/Characters/";
+    private const string ANIMCLIP_PATH = "/AnimClips/";
+    private const string FBX_SUFFIX = ".fbx";
+
     [MenuItem("Game/Anim/ExtractAll")]
     static void ExtractAll()
     {
@@ -20,8 +26,32 @@ public class AnimClipExtractEditor : Editor
 
         Object obj = Selection.objects[0];
         string name = obj.name;
-        string path = AssetDatabase.GetAssetPath(obj);
+        //string fullname = AssetDatabase.GetAssetPath(obj);
+        GenerateAnim(name);
+    }
 
-        int i = 0;
+    /// <summary>
+    /// 生成一个动画资源
+    /// </summary>
+    /// <param name="name"></param>
+    /// <param name="path"></param>
+    private static void GenerateAnim(string fbxname)
+    {
+        string animclippath = PREFABS_PATH + fbxname + ANIMCLIP_PATH;
+        SplitAnimClip(animclippath, fbxname);
+    }
+
+    private static void SplitAnimClip(string path, string fbxname)
+    {
+        string srcpath = ANIM_PATH + "/" + fbxname;
+
+        var files = Directory.GetFiles(srcpath, "*.fbx");
+        foreach (var file in files)
+        {
+            int i = 0;
+        }
     }
 }
+
+
+
