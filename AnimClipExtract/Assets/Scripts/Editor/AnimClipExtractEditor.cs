384ac5c05b7b014f7e30ba86af283c8d20958030
谢刘建 <377624289@qq.com>
2017-05-26 23:20:02 +0800

    AnimClipExtract

--- AnimClipExtract/Assets/Scripts/Editor/AnimClipExtractEditor.cs
+++ AnimClipExtract/Assets/Scripts/Editor/AnimClipExtractEditor.cs
@@ -11,6 +11,11 @@ public class AnimClipExtractEditor : Editor
     private const string PREFABS_PATH = "Assets/Prefabs/3D/Characters/";
     private const string ANIMCLIP_PATH = "/AnimClips/";
     private const string FBX_SUFFIX = ".fbx";
+    private const string ANIM_SUFFIX = ".anim";
+    private const string PREAFAB_SUFFIX = ".prefab";
+    private const string INVALID_ANIM_NAME = "__preview__";
+
+    private const string FILE_ERROR = "不能选中动画文件生成！";
 
     [MenuItem("Game/Anim/ExtractAll")]
     static void ExtractAll()
@@ -25,9 +30,10 @@ public class AnimClipExtractEditor : Editor
             return;
 
         Object obj = Selection.objects[0];
-        string name = obj.name;
-        //string fullname = AssetDatabase.GetAssetPath(obj);
-        GenerateAnim(name);
+        if (obj == null)
+            return;
+
+        GenerateAnim(obj.name);
     }
 
     /// <summary>
@@ -37,19 +43,65 @@ public class AnimClipExtractEditor : Editor
     /// <param name="path"></param>
     private static void GenerateAnim(string fbxname)
     {
-        string animclippath = PREFABS_PATH + fbxname + ANIMCLIP_PATH;
-        SplitAnimClip(animclippath, fbxname);
+        SplitAnimClip(fbxname);
+        CreatePrefab(fbxname);
     }
 
-    private static void SplitAnimClip(string path, string fbxname)
+    private static void SplitAnimClip(string fbxname)
     {
-        string srcpath = ANIM_PATH + "/" + fbxname;
+        string srcpath = ANIM_PATH + fbxname;
+        string clippath = PREFABS_PATH + fbxname + ANIMCLIP_PATH;
+
+        DirectoryInfo dirinfo = new DirectoryInfo(srcpath);
+        if (!dirinfo.Exists)
+        {
+            Debug.LogError(FILE_ERROR);
+            return;
+        }
+
+        dirinfo = new DirectoryInfo(clippath);
+        if (!dirinfo.Exists)
+        {
+            Directory.CreateDirectory(clippath);
+        }
 
         var files = Directory.GetFiles(srcpath, "*.fbx");
         foreach (var file in files)
         {
-            int i = 0;
+            if (!file.Contains("@"))
+                continue;
+
+            Object[] clipobjs = AssetDatabase.LoadAllAssetsAtPath(file);
+            if (clipobjs.Length <= 0)
+                continue;
+
+            foreach (var clipobj in clipobjs)
+            {
+                AnimationClip srcclip = clipobj as AnimationClip;
+                if (srcclip == null)
+                    continue;
+
+                if (srcclip.name.Contains(INVALID_ANIM_NAME))
+                    continue;
+
+                string dstclippath = clippath + srcclip.name + ANIM_SUFFIX;
+                AnimationClip dstclip = AssetDatabase.LoadAssetAtPath(dstclippath, typeof(AnimationClip)) as AnimationClip;
+                if (dstclip != null)
+                    AssetDatabase.DeleteAsset(dstclippath);
+
+                AnimationClip tempclip = new AnimationClip();
+                EditorUtility.CopySerialized(srcclip, tempclip);
+                AssetDatabase.CreateAsset(tempclip, dstclippath);
+            }
         }
+    }
+
+    private static void CreatePrefab(string fbxname)
+    {
+        string srcpath = ANIM_PATH + fbxname + "/" + fbxname + FBX_SUFFIX;
+        string dstpath = PREFABS_PATH + fbxname + "/" + fbxname + PREAFAB_SUFFIX;
+        GameObject obj = AssetDatabase.LoadAssetAtPath(srcpath, typeof(GameObject)) as GameObject;
+        PrefabUtility.CreatePrefab(dstpath, obj);
     }
 }
 
