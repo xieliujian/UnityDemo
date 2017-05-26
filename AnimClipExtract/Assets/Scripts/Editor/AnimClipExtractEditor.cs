68bb73019992bcbb5aa34efb7677bef0d60d2c84
谢刘建 <377624289@qq.com>
2017-05-26 08:27:15 +0800

    AnimClipExtract

---
+++ AnimClipExtract/Assets/Scripts/Editor/AnimClipExtractEditor.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class AnimClipExtractEditor : Editor
+{
+    [MenuItem("Game/Anim/ExtractAll")]
+    static void ExtractAll()
+    {
+
+    }
+
+    [MenuItem("Game/Anim/ExtractOne")]
+    static void ExtractOne()
+    {
+
+    }
+}
