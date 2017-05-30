09d6cfa02458f7133d59352e852bb078b1ff5602
谢刘建 <377624289@qq.com>
2017-05-31 00:39:08 +0800

    UIModelShow角色背后添加场景测试

--- UIModelShow/Assets/Scripts/UIModelShow.cs
+++ UIModelShow/Assets/Scripts/UIModelShow.cs
@@ -6,7 +6,15 @@ public class UIModelShow : MonoBehaviour
 {
     public Transform mRole;
 
+    public Animator mAnimator;
+
     public UIWidget mTouch;
+
+    public UISprite mStunBtn;
+
+    public UISprite mDeadBtn;
+
+    public UISprite mIdleBtn;
 
     #region 内置函数
 
@@ -14,6 +22,9 @@ public class UIModelShow : MonoBehaviour
     void Start ()
     {
         UIEventListener.Get(mTouch.gameObject).onDrag = OnTouchDrag;
+        UIEventListener.Get(mStunBtn.gameObject).onClick = OnStunBtnClick;
+        UIEventListener.Get(mDeadBtn.gameObject).onClick = OnDeadBtnClick;
+        UIEventListener.Get(mIdleBtn.gameObject).onClick = OnIdleBtnClick;
     }
 	
 	// Update is called once per frame
@@ -29,6 +40,22 @@ public class UIModelShow : MonoBehaviour
     {
         float yrot = mRole.localEulerAngles.y - (delta.x * 0.5f);
         mRole.localEulerAngles = new Vector3(mRole.localEulerAngles.x , yrot, mRole.localEulerAngles.z);
+    }
+
+    private void OnStunBtnClick(GameObject go)
+    {
+        mAnimator.SetBool("stun", true);
+    }
+
+    private void OnDeadBtnClick(GameObject go)
+    {
+        mAnimator.SetBool("dead", true);
+    }
+
+    private void OnIdleBtnClick(GameObject go)
+    {
+        mAnimator.SetBool("stun", false);
+        mAnimator.SetBool("dead", false);
     }
 
     #endregion
