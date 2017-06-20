4e819658bb371ee357a35905d5900ba5c4f0f4ac
谢刘建 <377624289@qq.com>
2017-06-20 22:00:46 +0800

    ThirdPersonCam可以用2个手指缩放摄像机

--- ThirdPersonCam/Assets/Scripts/Launcher.cs
+++ ThirdPersonCam/Assets/Scripts/Launcher.cs
@@ -4,16 +4,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using HedgehogTeam.EasyTouch;
 
-public class Launcher : MonoBehaviour {
-
-    public Camera mUICamera;
-
+public class Launcher : MonoBehaviour
+{
 	// Use this for initialization
 	void Start ()
     {
         //EasyTouch.instance.enabledNGuiMode = true;
         EasyTouch.Set3DPickableLayer(1 << LayerMask.NameToLayer("UI"));
-        EasyTouch.AddCamera(mUICamera, true);
+        EasyTouch.AddCamera(UICamera.mainCamera, true);
     }
 	
 	// Update is called once per frame
