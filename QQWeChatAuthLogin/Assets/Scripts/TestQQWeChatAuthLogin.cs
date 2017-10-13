51c3535b194199c8da7bb45c4365e4acb36d829e
谢刘建 <377624289@qq.com>
2017-10-12 19:17:03 -0500

    QQWeChatAuthLogin 1, 完成qq在android登录

--- QQWeChatAuthLogin/Assets/Scripts/TestQQWeChatAuthLogin.cs
+++ QQWeChatAuthLogin/Assets/Scripts/TestQQWeChatAuthLogin.cs
@@ -5,8 +5,47 @@ using UnityEngine;
 using UnityEngine.UI;
 using cn.sharesdk.unity3d;
 
+public class WechatData
+{
+    public string token;
+    public string openID;
+    public string unionID;
+}
+
+public class QQData
+{
+    public string token;
+}
+
+/// <summary>
+/// QQ的web认证的数据
+/// </summary>
+public class QQWebAuthData
+{
+    public string openid;
+    public string unionid;
+}
+
+/// <summary>
+/// QQ的web认证的错误数据
+/// </summary>
+public class QQWebAuthError
+{
+    public string error;
+    public string error_description;
+}
+
 public class TestQQWeChatAuthLogin : MonoBehaviour
 {
+    #region 常量
+
+    /// <summary>
+    /// QQ认证地址格式
+    /// </summary>
+    public static string QQWebAuthFormat = "https://graph.qq.com/oauth2.0/me?access_token={0}&unionid=1";
+
+    #endregion
+
     #region 变量
 
     /// <summary>
@@ -81,15 +120,52 @@ public class TestQQWeChatAuthLogin : MonoBehaviour
             if (type == PlatformType.QQ)
             {
                 Hashtable datatemp = mShareSDK.GetAuthInfo(type);
-                string qqdata = datatemp.toJson();
-                Debug.Log("OnAuthHandler qqdata : " + qqdata);
+                Debug.Log("OnAuthHandler qqdata : " + datatemp.toJson());
+
+                QQData qqdata = JsonUtility.FromJson<QQData>(datatemp.toJson());
+                StartCoroutine(QQWebAuth(qqdata));
             }
             else if (type == PlatformType.WeChat)
             {
                 Hashtable datatemp = mShareSDK.GetAuthInfo(type);
-                string wechatdata = datatemp.toJson();
-                Debug.Log("OnAuthHandler wechatdata : " + wechatdata);
+                Debug.Log("OnAuthHandler wechatdata : " + datatemp.toJson());
+
+                WechatData wechatdata = JsonUtility.FromJson<WechatData>(datatemp.toJson());
+                string token = wechatdata.token;
+                string openid = wechatdata.openID;
+                string unionid = wechatdata.unionID;
+                Debug.Log("OnAuthHandler wechatdata token : " + token + " openid : " + openid + " unionid : " + unionid);
             }
+        }
+    }
+    
+    IEnumerator QQWebAuth(QQData qqdata)
+    {
+        string token = qqdata.token;
+        string authpath = string.Format(QQWebAuthFormat, token);
+        WWW www = new WWW(authpath);
+        yield return www;
+
+        string qqauthdatatemp = www.text;
+        qqauthdatatemp = qqauthdatatemp.Replace("callback(", "");
+        qqauthdatatemp = qqauthdatatemp.Replace(");", "");
+        Debug.Log("QQWebAuth 1 : " + qqauthdatatemp);
+
+        QQWebAuthError qqautherror = JsonUtility.FromJson<QQWebAuthError>(qqauthdatatemp);
+        if (qqautherror.error != null)
+        {
+            Debug.Log("QQWebAuth error");
+
+            if (qqautherror.error_description != null)
+                Debug.Log("QQWebAuth errordesc : " + qqautherror.error_description);
+        }
+        else
+        {
+            QQWebAuthData qqauthdata = JsonUtility.FromJson<QQWebAuthData>(qqauthdatatemp);
+            token = qqdata.token;
+            string openid = qqauthdata.openid;
+            string unionid = qqauthdata.unionid;
+            Debug.Log("QQWebAuth 2 : token : " + token + " openid : " + openid + " unionid : " + unionid);
         }
     }
 
